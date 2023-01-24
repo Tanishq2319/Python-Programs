852b60f79e4e80df8519b141ed2b3d54a9ddaea1
Tanishq <104089429+Tanishq2319@users.noreply.github.com>
2023-01-24 13:15:16 +0530

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -5,8 +5,6 @@ def count_vowels(string):
         if i in vowels:
             count += 1
     return count
-
-# Usage example
 string=input("enter your string")
 vowel_count = count_vowels(string)
 print("Number of vowels in '" + string + "' is: " + str(vowel_count))
